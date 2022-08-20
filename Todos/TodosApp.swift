78f58bc791d51824d3199ad35f3bb3459d21df51
Falko Schumann <falko.schumann@muspellheim.de>
2022-08-20 17:42:02 +0200

    build: init project with Xcode wizard

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Falko Schumann on 20.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
